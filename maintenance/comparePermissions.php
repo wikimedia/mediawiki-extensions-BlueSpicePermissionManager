d6d4d75fa51b47460b1a2eff7de298b0ea979d17
pwirth <wirth@hallowelt.biz>
2019-11-20 11:50:56 +0100

    Use BlueSpice\Services instead of MediaWiki\MediaWikiServices.
    => Requires: I4168373f1597e7dbc6334ffa8f482ceca9eba494
    
    ERM:16775
    [3.2]
    
    Change-Id: I5a17f9cf748bcd2eb63887d908b60e5181ec1cd5

--- maintenance/comparePermissions.php
+++ maintenance/comparePermissions.php
@@ -1,5 +1,7 @@
 <?php
 
+use BlueSpice\Services;
+
 $IP = dirname( dirname( dirname( __DIR__ ) ) );
 
 require_once "$IP/maintenance/Maintenance.php";
@@ -16,9 +18,7 @@ class comparePermissions extends \Maintenance {
 		$oldPermissions = $this->getConfig()->get( 'GroupPermissions' );
 
 		// Initialize and apply roles
-		$roleManager = \MediaWiki\MediaWikiServices::getInstance()->getService(
-			'BSRoleManager'
-		);
+		$roleManager = Services::getInstance()->getBSRoleManager();
 		if ( $roleManager->isRoleSystemEnabled() == false ) {
 			$roleManager->enableRoleSystem();
 			$roleManager->applyRoles();
@@ -27,9 +27,7 @@ class comparePermissions extends \Maintenance {
 		// Read new permissions
 		$newPermissions = $this->getConfig()->get( 'GroupPermissions' );
 
-		$permissionRegistry = \MediaWiki\MediaWikiServices::getInstance()->getService(
-			'BSPermissionRegistry'
-		);
+		$permissionRegistry = Services::getInstance()->getBSPermissionRegistry();
 
 		// Compile results
 		$result = [];
