a24482e1c8f1535fb45b352f23886b83b04eeab2
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 13:08:28 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: I95804a1488ebdc3295d5965504b3f380118c3552

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace BlueSpice\PermissionManager;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-permissionmanager';
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'permissionmanager-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'PermissionManager' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bs-permissionmanager-label' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-permissionmanager-desc' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bs-permissionmanager-label' );
+	}
+}
---
+++ src/HookHandler/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\PermissionManager\HookHandler;
+
+use BlueSpice\PermissionManager\GlobalActionsManager;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsManager',
+			[
+				'special-bluespice-permissionmanager' => [
+					'factory' => static function () {
+						return new GlobalActionsManager();
+					}
+				]
+			]
+		);
+	}
+}
