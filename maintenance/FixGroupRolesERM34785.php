04a8e6642143f7192f89095e85720c7cb2f885d6
rvogel <vogel@hallowelt.biz>
2024-01-10 13:28:13 +0100

    Add maintenance script to fix dynamic config issue
    ERM34785
    
    [4.4.x]
    
    Change-Id: I5d2a4f0be6fa03e57e63a113329e4155ad0ed909

---
+++ maintenance/FixGroupRolesERM34785.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once __DIR__ . '/../../../maintenance/Maintenance.php';
+
+class FixGroupRolesERM34785 extends Maintenance {
+
+	public function execute() {
+		$db = $this->getDB( DB_PRIMARY );
+		$row = $db->selectRow(
+			'mwstake_dynamic_config',
+			[ 'mwdc_serialized', 'mwdc_timestamp' ],
+			[
+				'mwdc_key' => 'bs-permissionmanager-roles',
+				'mwdc_is_active' => 1
+			],
+			__METHOD__
+		);
+
+		if ( $row === false ) {
+			$this->output( "No entry for PermissionManagerFound\n" );
+			return;
+		}
+
+		$config = FormatJson::decode( $row->mwdc_serialized, true );
+		if ( !isset( $config['bsgGroupRoles'] ) ) {
+			$this->output( "No bsgGroupRoles found\n" );
+			return;
+		}
+
+		$this->output( "CURRENT CONFIG:\n" );
+		$this->output( FormatJson::encode( $config, true ) . "\n" );
+
+		$fixedGroupRoles = [];
+		foreach ( $config['bsgGroupRoles'] as $groupName => $roleAssignments ) {
+			if ( $roleAssignments === null ) {
+				$fixedGroupRoles[] = $groupName;
+				unset( $config['bsgGroupRoles'][$groupName] );
+			}
+		}
+
+		if ( empty( $fixedGroupRoles ) ) {
+			$this->output( "Nothing found to be fixed\n" );
+			return;
+		}
+
+		$this->output( "FIXED CONFIG:\n" );
+		$this->output( FormatJson::encode( $config, true ) . "\n" );
+
+		$serialized = FormatJson::encode( $config );
+
+		// Disable broken config
+		$res = $db->update(
+			'mwstake_dynamic_config',
+			[ 'mwdc_is_active' => 0 ],
+			[
+				'mwdc_key' => 'bs-permissionmanager-roles',
+				'mwdc_timestamp' => $row->mwdc_timestamp,
+			],
+			__METHOD__
+		);
+		if ( $res === false ) {
+			$this->output( "Update failed\n" );
+			return;
+		}
+
+		// Insert and activate fixed config
+		$res = $db->insert(
+			'mwstake_dynamic_config',
+			[
+				'mwdc_key' => 'bs-permissionmanager-roles',
+				'mwdc_serialized' => $serialized,
+				'mwdc_timestamp' => wfTimestampNow(),
+				'mwdc_is_active' => 1
+			],
+			__METHOD__
+		);
+		if ( $res === false ) {
+			$this->output( "Update failed\n" );
+			return;
+		}
+
+		$fixedGroupRoles = implode( ', ', $fixedGroupRoles );
+		$this->output( "Fixed GroupRoles: $fixedGroupRoles" );
+	}
+}
+
+$maintClass = FixGroupRolesERM34785::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
