a144c4abc9d1407a187952d350cc2d37fcb11010
hslater <hamish.slater@hallowelt.com>
2025-01-28 14:53:17 +0100

    Update SpecialPage namespace
    ERM38444
    
    [REL1_43, master]

--- src/AdminTool.php
+++ src/AdminTool.php
@@ -4,6 +4,7 @@ namespace BlueSpice\PermissionManager;
 
 use BlueSpice\IAdminTool;
 use MediaWiki\Message\Message;
+use MediaWiki\SpecialPage\SpecialPage;
 
 class AdminTool implements IAdminTool {
 
@@ -12,7 +13,7 @@ class AdminTool implements IAdminTool {
 	 * @return string
 	 */
 	public function getURL() {
-		$tool = \SpecialPage::getTitleFor( 'PermissionManager' );
+		$tool = SpecialPage::getTitleFor( 'PermissionManager' );
 		return $tool->getLocalURL();
 	}
 
